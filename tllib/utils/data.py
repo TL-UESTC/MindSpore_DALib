e20ac72edb2dfd006a755cedf69cc4586238e338
AdamsLiuG <1574366968@qq.com>
2023-12-15 17:08:54 +0800

    do some changes

--- tllib/utils/data.py
+++ tllib/utils/data.py
@@ -9,6 +9,7 @@ import numpy as np
 import mindspore
 import mindspore.ops as ops
 from mindspore.dataset import Sampler
+from msadapter.pytorch.utils.data import Sampler
 from mindspore.dataset import GeneratorDataset
 from msadapter.pytorch.utils.data import Dataset
 # from torch.utils.data import DataLoader, Dataset
@@ -17,52 +18,23 @@ from typing import TypeVar, Iterable, Dict, List
 T_co = TypeVar('T_co', covariant=True)
 T = TypeVar('T')
 
-
-def send_to_device(tensor, device):
-    """
-    Recursively sends the elements in a nested list/tuple/dictionary of tensors to a given device.
-
-    Args:
-        tensor (nested list/tuple/dictionary of :obj:`torch.Tensor`):
-            The data to send to a given device.
-        device (:obj:`torch.device`):
-            The device to send the data to
-
-    Returns:
-        The same data structure as :obj:`tensor` with all tensors sent to the proper device.
-    """
-    if isinstance(tensor, (list, tuple)):
-        return type(tensor)(send_to_device(t, device) for t in tensor)
-    elif isinstance(tensor, dict):
-        return type(tensor)({k: send_to_device(v, device) for k, v in tensor.items()})
-    elif not hasattr(tensor, "to"):
-        return tensor
-    return tensor.to(device)
-
-
 class ForeverDataIterator:
     r"""A data iterator that will never stop producing data"""
 
-    def __init__(self, data_loader: GeneratorDataset, device=None):
+    def __init__(self, data_loader: GeneratorDataset):
         self.data_loader = data_loader
         self.iter = iter(self.data_loader)
-        self.device = device
 
     def __next__(self):
         try:
             data = next(self.iter)
-            if self.device is not None:
-                data = send_to_device(data, self.device)
         except StopIteration:
             self.iter = iter(self.data_loader)
             data = next(self.iter)
-            if self.device is not None:
-                data = send_to_device(data, self.device)
         return data
 
     def __len__(self):
         return len(self.data_loader)
-
 
 class RandomMultipleGallerySampler(Sampler):
     r"""Sampler from `In defense of the Triplet Loss for Person Re-Identification
@@ -169,7 +141,7 @@ def concatenate(tensors):
     ``tensors`` can be :class:`torch.Tensor`, List or Dict, but they must be the same data format.
     """
     if isinstance(tensors[0], mindspore.Tensor):
-        return ops.cat(tensors, dim=0)
+        return ops.concat(tensors, axis=0)
     elif isinstance(tensors[0], List):
         ret = []
         for i in range(len(tensors[0])):
